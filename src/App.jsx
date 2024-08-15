73727b93acc019c3715ef8e9a9644f15e6894662
sujit patel <sujitworkclg@gmail.com>
2024-08-15 21:25:43 +0530

    add new img

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import whiteImage from './assets/white.jpg';
 import greenImage from './assets/green.jpg';
 import yellowImage from './assets/yellow.jpg';
 import blueImage from './assets/blue.jpg';
-import pinkImage from './assets/pink.jpg';
+import pinkImage from './assets/light-pink.jpg';
 import blackImage from './assets/black.jpg';
 import skyImage from './assets/sky.jpg';
 
 function App() {
-  const [background, setBackground] = useState({color: "skyblue", image: skyImage});
+  const [background, setBackground] = useState({image: skyImage});
 
-  const setcolor = (color, image) => {
-    setBackground({color, image});
+  const setcolor = (image) => {
+    setBackground({image});
   };
 
   return (
@@ -30,28 +30,28 @@ function App() {
             
             {/* buttons */}
             <button className='bg-orange-500 text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("orange", orangeImage)}>Orange</button>
+              onClick={() => setcolor(orangeImage)}>Orange</button>
             
             <button className='bg-slate-400 text-blue-500 font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("white", whiteImage)}>White</button>
+              onClick={() => setcolor(whiteImage)}>White</button>
             
             <button className='bg-green-500 text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("#22c44e", greenImage)}>Green</button>
+              onClick={() => setcolor(greenImage)}>Green</button>
             
             <button className='bg-yellow-400 text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("yellow", yellowImage)}>Yellow</button>
+              onClick={() => setcolor(yellowImage)}>Yellow</button>
             
             <button className='bg-blue-600 text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("blue", blueImage)}>Blue</button>
+              onClick={() => setcolor(blueImage)}>Blue</button>
             
             <button className='bg-pink-500 text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("#e75480", pinkImage)}>Pink</button>
+              onClick={() => setcolor(pinkImage)}>Pink</button>
 
             <button className='bg-black text-white font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("black", blackImage)}>Black</button>
+              onClick={() => setcolor(blackImage)}>Black</button>
 
             <button className='bg-blue-400 text-black font-bold rounded-xl px-2 py-1'
-              onClick={() => setcolor("#87CEEB", skyImageImage)}>SkyBlue</button>
+              onClick={() => setcolor(skyImage)}>SkyBlue</button>
           
           </div>
 
